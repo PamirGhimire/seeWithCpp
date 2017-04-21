c5bc622bf5d6696a38eaec82e525c27ce0fb9382
Pamir_Ghimire <pamirghimire@gmail.com>
2017-04-21 17:39:42 +0200

    added color levels reduction, s&p noise not added in place anymore

--- code/processes.cpp
+++ code/processes.cpp
@@ -1,7 +1,12 @@
 #include<processes.h>
 
+// add salt and pepper noise
+void fn_swcAddSaltAndPepper(const cv::Mat &inputim, cv::Mat& outputim, int npixels){
 
-void fn_swcAddSaltAndPepper(cv::Mat &inputim, int npixels){
+    // clone the input image to the output location, only if locations are different
+    if (&inputim != &outputim){
+        outputim = inputim.clone();
+    }
 
     // for specified number of pixels that are to be corrupted
     for (int i = 0; i<npixels; i++){
@@ -18,27 +23,56 @@ void fn_swcAddSaltAndPepper(cv::Mat &inputim, int npixels){
             // uni-channel image
             if (sp == 0){
                 // add salt
-                inputim.at<uchar>(v, u) = 255;
+                outputim.at<uchar>(v, u) = 255;
             }else{
                 // add pepper
-                inputim.at<uchar>(v, u) = 0;
+                outputim.at<uchar>(v, u) = 0;
             }
         }
         else if (inputim.channels() == 3){
             // multi-channel image
             if (sp == 0){
                 // add salt
-                inputim.at<cv::Vec3b>(v, u) [0] = 255;
-                inputim.at<cv::Vec3b>(v, u) [1] = 255;
-                inputim.at<cv::Vec3b>(v, u) [2] = 255;
+                outputim.at<cv::Vec3b>(v, u) [0] = 255;
+                outputim.at<cv::Vec3b>(v, u) [1] = 255;
+                outputim.at<cv::Vec3b>(v, u) [2] = 255;
 
             }else{
                 // add pepper
-                inputim.at<cv::Vec3b>(v, u) [0] = 0;
-                inputim.at<cv::Vec3b>(v, u) [1] = 0;
-                inputim.at<cv::Vec3b>(v, u) [2] = 0;
+                outputim.at<cv::Vec3b>(v, u) [0] = 0;
+                outputim.at<cv::Vec3b>(v, u) [1] = 0;
+                outputim.at<cv::Vec3b>(v, u) [2] = 0;
             }
 
         }
     }
 }
+
+//--------------------------------------------------------------------------
+// reduce number of colors, new unit = graystep (instead of unit = 1)
+// image traversed using pointers
+
+void fn_swcReduceColors(const cv::Mat &inputim, cv::Mat& outputim, int graystep)
+{
+    // clone the input image to the output location, only if locations are different
+    if (&inputim != &outputim){
+        outputim = inputim.clone();
+    }
+
+    // number of columns in the image
+    int ncols = inputim.cols * inputim.channels();
+
+    // for each row in the image
+    for (int nrow = 0; nrow < inputim.rows; nrow++){
+        // get the pointer to the first element of the row
+        uchar* rowdata = outputim.ptr<uchar>(nrow);
+
+        // for each column in the row
+        for (int ncol = 0; ncol < ncols; ncol++){
+            // apply process
+            rowdata[ncol] = nearbyint ( (1.00 * (rowdata[ncol])) / graystep ) * graystep;
+        }
+    }
+
+}
+
--- code/processes.h
+++ code/processes.h
@@ -3,14 +3,17 @@
 //---------------------------------------------------------
 #include <stdio.h>
 #include<iostream>
+#include<math.h>
 #include<qdebug.h>
 
 #include<opencv2/core/core.hpp>
 #include<opencv2/imgproc/imgproc.hpp>
 
 // add salt and pepper noise to the image
-void fn_swcAddSaltAndPepper(cv::Mat &inputim, int npixels);
+void fn_swcAddSaltAndPepper(const cv::Mat &inputim, cv::Mat& outputim, int npixels);
 
+// reduce the number of colors
+void fn_swcReduceColors(const cv::Mat &inputim, cv::Mat& outputim, int graystep);
 
 
 
